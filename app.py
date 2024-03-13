2e3cb9f6ac4c69cb6c4ff372e1e233a74eca717c
Ola Ekdahl <ola@ciracon.com>
2024-03-13 06:18:31 -0700

    added dockerfile and package action

--- app.py
+++ app.py
@@ -95,4 +95,6 @@ def search_boardgames():
     return jsonify(games)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    # app.run(host='0.0.0.0')
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
